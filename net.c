eb99957cd7bdc61e173b72dfbaed9e46c6ac8d3e
nac-39 <83295760+nac-39@users.noreply.github.com>
2023-08-24 22:03:05 +0900

    step1: fix

--- net.c
+++ net.c
@@ -55,7 +55,7 @@ static int net_device_close(struct net_device *dev) {
       return -1;
     }
   }
-  dev->flags &= -NET_DEVICE_FLAG_UP; // upフラグを落とす
+  dev->flags &= ~NET_DEVICE_FLAG_UP; // upフラグを落とす
   infof("dev=%s, state=%s", dev->name, NET_DEVICE_STATE(dev));
   return 0;
 }
